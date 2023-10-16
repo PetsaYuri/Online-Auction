8989bf8396560ac4317d20b4ec26dbb2b538d9e2
PetsaYuri <petsa.yuri@gmail.com>
2023-10-16 17:19:06 +0300

    CI/CD test5

--- src/main/java/com/OnlineAuction/Controllers/UserController.java
+++ src/main/java/com/OnlineAuction/Controllers/UserController.java
@@ -26,7 +26,7 @@ public class UserController {
 
     @GetMapping("/init")
     public String init() {
-        return "CI/CD deploy now????";
+        return "CI/CD deploy now???? And now?";
     }
 
     @GetMapping
